98398f0319fe0830ef9cfc944b51b6557f8a5f92
Marcos Vinicius Almeida Guimaraes <mvaguimaraes@gmail.com>
2017-07-13 11:21:57 -0300

    LuckyWheel
    Uploading app to github

---
+++ app/src/main/java/com/example/mvaguimaraes/luckywheel/HomeActivity.java
@@ -0,0 +1,110 @@
+package com.example.mvaguimaraes.luckywheel;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
+import android.content.Context;
+
+public class HomeActivity extends Activity
+{
+    Button btnSignIn,btnSignUp;
+    LoginDataBaseAdapter loginDataBaseAdapter;
+
+    SessionManager session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        session = new SessionManager(getApplicationContext());
+
+        // create a instance of SQLite Database
+        loginDataBaseAdapter=new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter=loginDataBaseAdapter.open();
+
+        // Get The Refference Of Buttons
+        btnSignIn=(Button)findViewById(R.id.buttonSignIN);
+        btnSignUp=(Button)findViewById(R.id.buttonSignUP);
+
+        // Set OnClick Listener on SignUp button
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                /// Create Intent for SignUpActivity  abd Start The Activity
+                Intent intentSignUP=new Intent(getApplicationContext(),SignUPActivity.class);
+                startActivity(intentSignUP);
+            }
+        });
+    }
+    // Methos to handleClick Event of Sign In Button
+    public void signIn(View V)
+    {
+        final Dialog dialog = new Dialog(HomeActivity.this);
+        dialog.setContentView(R.layout.login);
+        dialog.setTitle("Login");
+
+        // get the Refferences of views
+        final  EditText editTextUserName=(EditText)dialog.findViewById(R.id.editTextUserNameToLogin);
+        final  EditText editTextPassword=(EditText)dialog.findViewById(R.id.editTextPasswordToLogin);
+
+        Button btnSignIn=(Button)dialog.findViewById(R.id.buttonSignIn);
+
+        // Set On ClickListener
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // get The User name and Password
+                String userName=editTextUserName.getText().toString();
+                String password=editTextPassword.getText().toString();
+
+                // fetch the Password form database for respective user name
+                String storedPassword=loginDataBaseAdapter.getSinlgeEntry(userName);
+
+                // check if the Stored password matches with  Password entered by user
+                if(password.equals(storedPassword))
+                {
+                    Toast.makeText(HomeActivity.this, "Congrats: Login Successfull", Toast.LENGTH_LONG).show();
+                    dialog.dismiss();
+
+                    //Teste pra mandar o usuario
+
+                    session.createLoginSession("Welcome: ",userName);
+
+                    Intent i = new Intent(getApplicationContext(), wheel.class);
+                    i.putExtra("new_variable_name",userName);
+
+                    //Teste pra mandar o usuario
+
+                    Intent browserIntent =
+                            new Intent(HomeActivity.this, wheel.class);
+
+                    startActivity(browserIntent);
+                    startActivity(i);
+
+                }
+                else
+                {
+                    Toast.makeText(HomeActivity.this, "User Name or Password does not match", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        dialog.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Close The Database
+        loginDataBaseAdapter.close();
+    }
+}
